e8b9b9ab2947c89b3b12d7371bc03d3f74e9e539
Alina Kazantseva <henessy230@gmail.com>
2026-06-08 12:18:59 +0300

    Solution for 'Summary Ranges' Problem

---
+++ summary-ranges.swift
@@ -0,0 +1,38 @@
+class Solution {
+    func produceString(_ nums: [Int]) -> String {
+        guard nums.count > 1 else {
+            return "\(nums[0])"
+        }
+
+        let min = nums[0]
+        let max = nums[nums.count - 1]
+
+        return "\(min)->\(max)"
+    }
+
+    func summaryRanges(_ nums: [Int]) -> [String] {
+        guard !nums.isEmpty else {
+            return []
+        }
+
+        var ans: [String] = []
+        var currentSet: [Int] = []
+
+        for i in 0..<nums.count {
+            let num = nums[i]
+            currentSet.append(num)
+
+            guard i + 1 < nums.count else {
+                ans.append(produceString(currentSet))
+                break
+            }
+
+            if nums[i + 1] != (num + 1) {
+                ans.append(produceString(currentSet))
+                currentSet = []
+            }
+        }
+
+        return ans
+    }
+}
