cde2830158ed8686e7976c8ff0f0265b1bac9e75
Alina Kazantseva <henessy230@gmail.com>
2026-06-11 19:07:03 +0300

    Valid Palindrome

---
+++ valid-palindrome.swift
@@ -0,0 +1,30 @@
+
+import Foundation
+
+class Solution {
+    func isPalindrome(_ s: String) -> Bool {
+        let str = Array(
+            s
+            .lowercased()
+            .filter { $0.isLetter || $0.isNumber }
+        )
+
+        guard !str.isEmpty else {
+            return true
+        }
+        
+        var i = 0
+        var j = str.count - 1
+        
+        while i < j {
+            if str[i] != str[j] {
+                return false
+            }
+            
+            i += 1
+            j -= 1
+        }
+        
+        return true
+    }
+}
