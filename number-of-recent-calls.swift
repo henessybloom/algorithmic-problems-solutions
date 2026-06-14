5a482ef1d41265b987ef899df06a7c55b470d742
Alina Kazantseva <henessy230@gmail.com>
2026-06-15 00:19:21 +0300

    number-of-recent-calls

---
+++ number-of-recent-calls.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+class RecentCounter {
+    var callsQueue: [Int] = []
+
+    init() {
+        
+    }
+    
+    func ping(_ t: Int) -> Int {
+        guard !callsQueue.isEmpty else {
+            callsQueue.append(t)
+            return 1
+        }
+
+        let low = t - 3000
+        let high = t
+        var i = callsQueue.count - 1
+        var ans = 1
+
+        while i >= 0 {
+            let call = callsQueue[i]
+            
+            if call >= low && call <= high {
+                ans += 1
+                i -= 1
+                continue
+            } else {
+                callsQueue = Array(callsQueue[(i + 1)...])
+                break
+            }
+        }
+        
+        callsQueue.append(t)
+        
+        return ans
+    }
+}
