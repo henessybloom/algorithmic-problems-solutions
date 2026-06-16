1d09de218952ec34a248a562570bfba663dfe53f
Alina Kazantseva <henessy230@gmail.com>
2026-06-17 02:11:09 +0300

    consecutive-characters

---
+++ consecutive-characters.swift
@@ -0,0 +1,28 @@
+class Solution {
+    func maxPower(_ s: String) -> Int {
+        guard s.count > 1 else {
+            return 1
+        }
+
+        var lastSymbol = s[s.startIndex]
+        var ans = 0
+        var count = 0
+        var i = s.startIndex
+
+        while i < s.endIndex {
+            if s[i] == lastSymbol {
+                count += 1
+            } else {
+                ans = max(ans, count)
+                lastSymbol = s[i]
+                count = 1
+            }
+
+            i = s.index(after: i)
+        }
+
+        ans = max(ans, count)
+
+        return ans
+    }
+}
