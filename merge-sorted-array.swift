707618a6593942b197febd5476318f12a5413bcf
Alina Kazantseva <henessy230@gmail.com>
2026-06-17 19:29:48 +0300

    made the solution for 88 'merge-sorted-array' more readable

--- merge-sorted-array.swift
+++ merge-sorted-array.swift
@@ -3,29 +3,29 @@
 class Solution {
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
 
-        // last index of nums1
-        var last = m + n - 1
+        // write index of nums1
+        var write = m + n - 1
         var i = m - 1
         var j = n - 1
 
         // merge nums1 and nums2 in reverse order
         while i >= 0 && j >= 0 {
             if nums1[i] > nums2[j] {
-                nums1[last] = nums1[i]
+                nums1[write] = nums1[i]
                 i -= 1
             } else {
-                nums1[last] = nums2[j]
+                nums1[write] = nums2[j]
                 j -= 1
             }
 
-            last -= 1
+            write -= 1
         }
         
         // fill nums1 with leftovers
         while j >= 0 {
-            nums1[last] = nums2[j]
+            nums1[write] = nums2[j]
             j -= 1
-            last -= 1
+            write -= 1
         }
     }
 }
