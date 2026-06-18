fa9985714b7d69266e71875f827fe3485ebfea83
Alina Kazantseva <henessy230@gmail.com>
2026-06-18 04:38:34 +0300

    is-subsequence

---
+++ is-subsequence.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func isSubsequence(_ s: String, _ t: String) -> Bool {
+        var i = s.startIndex
+        var j = t.startIndex
+
+        while i < s.endIndex && j < t.endIndex {
+            if s[i] == t[j] {
+                i = s.index(after: i)
+            }
+
+            j = t.index(after: j)
+        }
+
+        return !(i < s.endIndex)
+    }
+}
