b176562f38f95358b3360cd619cb8c8d5acc774d
Alina Kazantseva <henessy230@gmail.com>
2026-06-25 17:53:40 +0300

    solved 1493 again and changed it's  title to the one which represents the solution more meticulously

--- longest-subarray-of-1's-after-deleting-one-element.swift
+++ longest-subarray-of-1's-after-deleting-one-element.swift
@@ -1,41 +1,42 @@
-//Key Idea: Maintaining a window with at most one zero by tracking consecutive ones before and after the zero.
+// Key Idea: Maintain two chunks of 1's, left and right and keep counting zeros. After hitting every second zero, move rightChunk to the leftChunk, set zeroCount to 1 and keep counting. At the end compare ans and sum of leftChunk and rightChunk once again to count the leftovers and return ans only if at least one zero was met. Otherwise return `nums.count - 1`
 
 class Solution {
     func longestSubarray(_ nums: [Int]) -> Int {
         guard nums.count > 1 else {
             return 0
         }
-        
+
+        var leftChunk = 0
+        var rightChunk = 0
         var ans = 0
-        var zerosCnt = 0
-        var leftSum = 0
-        var rightSum = 0
-
-        for i in 0..<nums.count {
+        var i = 0
+        var zeroCount = 0
+        
+        while i < nums.count {
             if nums[i] == 1 {
-                if zerosCnt == 0 {
-                    leftSum += 1
+                if zeroCount == 1 {
+                    rightChunk += 1
+                } else {
+                    leftChunk += 1
                 }
-
-                if zerosCnt == 1 {
-                    rightSum += 1
-                }
+                i += 1
+                continue
             }
 
-            if nums[i] == 0 {
-                zerosCnt += 1
+            zeroCount += 1
 
-                if zerosCnt == 2 {
-                    ans = max(ans, leftSum + rightSum)
-                    leftSum = rightSum
-                    rightSum = 0
-                    zerosCnt = 1
-                }
+            if zeroCount == 2 {
+                ans = max(leftChunk + rightChunk, ans)
+                leftChunk = rightChunk
+                rightChunk = 0
+                zeroCount = 1
             }
+
+            i += 1
         }
 
-        ans = max(ans, leftSum + rightSum)
+        ans = max(ans, leftChunk + rightChunk)
 
-        return zerosCnt == 0 ? nums.count - 1 : ans
+        return zeroCount == 0 ? nums.count - 1 : ans
     }
 }
