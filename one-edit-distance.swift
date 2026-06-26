cc1d77a1d6d462afc3c4f5a6ebec34cccea0cc33
Alina Kazantseva <henessy230@gmail.com>
2026-06-26 13:54:59 +0300

    Changed the solution for 161, made it more clear and shorter

--- one-edit-distance.swift
+++ one-edit-distance.swift
@@ -1,54 +1,27 @@
 //Key Idea: Scan both strings together and tolerate exactly one mismatch — if lengths differ by one, skip a character in the longer string; otherwise, allow a single replacement.
 
-import Foundation
-
 class Solution {
-    func isOneEditDistance(_ s: String, _ t: String) -> Bool {
-        if s.count > t.count {
-            isOneEditDistance(t, s)
+    func oneEditDistance(_ s: String, _ t: String) -> Bool {
+        guard s.count <= t.count else {
+            return oneEditDistance(t, s)
         }
-        
-        if s.isEmpty {
-            return t.count == 1
-        }
-        
-        if t.isEmpty {
-            return s.count == 1
-        }
-        
         let diff = abs(t.count - s.count)
-        
-        if diff > 1 {
-            return false
-        }
-        
-        var cnt = 0
+        var editCnt = 0
         var i = s.startIndex
         var j = t.startIndex
         
         while i < s.endIndex && j < t.endIndex {
             if s[i] != t[j] {
-                cnt += 1
-                
-                if cnt > 1 {
-                    return false
-                }
-                
-                if diff == 1 {
-                    j = t.index(after: j)
-                    continue
-                }
+                editCnt += 1
+                j = t.index(after: j)
+                continue
             }
             
             i = s.index(after: i)
             j = t.index(after: j)
         }
         
-        if cnt == 0 && diff == 1 {
-            return true
-        }
-        
-        return cnt == 1
+        return editCnt == 0 ? diff == 1 : editCnt == 1
     }
 }
 
